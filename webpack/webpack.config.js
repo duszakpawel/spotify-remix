b9d806d5eab8f8b9c6b672da74d4454391f8ea16
Pablo <duszakpawel@gmail.com>
2024-01-08 12:00:00 +0100

    Add webpack.config.json file

---
+++ webpack/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	mode: "production",
+	entry: {
+		background: path.resolve(__dirname, "..", "src", "background.ts"),
+	},
+	output: {
+		path: path.join(__dirname, "../dist"),
+		filename: "[name].js",
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: "*.css", to: ".", context: path.resolve(__dirname, "..", "src") },
+				{ from: "contentScript.js", to: ".", context: path.resolve(__dirname, "..", "src") },
+				{ from: ".", to: ".", context: "public" }
+			]
+		}),
+	],
+	optimization: {
+        minimize: false, // Wyłączenie minifikacji
+    }
+};
